8be0b311d4cbf600a03446305ad8efff8c0614ab
Alex Brackenridge <a.brack@live.co.uk>
2023-01-31 16:39:54 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import api from './api/axiosConfig'
+import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
-
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+//import Reviews from './components/reviews/Reviews';
+//import NotFound from './components/notFound/NotFound';
 
 function App() {
 
@@ -48,16 +52,21 @@ function App() {
 
   }
 
-useEffect(() => {
-  getMovies();
-}, [])
+  useEffect(() => {
+    getMovies();
+  },[])
 
   return (
     <div className="App">
-
+      <Header/>
       <Routes>
-        <Route path="/" element={<Layout />} /> 
-      </Routes> 
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            {/* <Route path="*" element = {<NotFound/>}></Route> */}
+          </Route>
+      </Routes>
 
     </div>
   );
